82c46db7756132e20c64ae349d9a72c2f8026235
i-am-sanguine <bhadmusademola.1@gmail.com>
2017-12-15 20:06:20 +0100

    Updated entire project files into existing repo

---
+++ src/writingReview/LoginProcedure.java
@@ -0,0 +1,50 @@
+package writingReview;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class LoginProcedure {
+    WebDriver driver ;
+
+
+    public LoginProcedure(WebDriver driver1) {
+		this.driver = driver1;
+	}
+
+
+	public  void login ( ) throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver",
+                "C:\\Users\\Ademola Bhadmus\\Documents\\HTC\\Learning Folder\\Hover and Rating\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+        driver.get("https://wallethub.com/join/light");
+        driver.manage().window().maximize();
+    //driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        System.out.println(driver.getTitle());
+         System.out.println(driver.getCurrentUrl());
+
+    WebElement login_tab = driver.findElement(By.xpath("/html/body/main/div/form/div/nav/ul/li[2]/a"));
+    login_tab.click();
+
+    WebElement email = driver.findElement(By.xpath("/html/body/main/div/form/div/div[1]/input"));
+    WebElement password = driver.findElement(By.xpath("/html/body/main/div/form/div/div[2]/input"));
+    email.clear();
+    email.sendKeys("ademolabhadmus@hotmail.com");
+
+
+
+    password.clear();
+    password.sendKeys("Qwerty123@");
+
+    WebElement login_button = driver.findElement(By.xpath("/html/body/main/div/form/div/div[3]/button"));
+    login_button.click();
+
+    Thread.sleep(20000);
+
+
+
+    }
+
+
+}
